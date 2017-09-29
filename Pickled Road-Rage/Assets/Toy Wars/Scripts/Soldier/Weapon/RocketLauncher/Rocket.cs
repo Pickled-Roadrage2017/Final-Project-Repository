0a40c9df0f484083915bb7e7c88ace22b77f3140
CallanDavies <callan.davies1@gmail.com>
2017-09-29 19:42:27 +1000

    Added update in the Rocket class, and a "RocketLauncher" tag in the tagmanager.

--- Pickled Road-Rage/Assets/Toy Wars/Scripts/Soldier/Weapon/RocketLauncher/Rocket.cs
+++ Pickled Road-Rage/Assets/Toy Wars/Scripts/Soldier/Weapon/RocketLauncher/Rocket.cs
@@ -2,29 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : Weapon {
 
     Rigidbody m_rbRocket;
+    // pointer to the RocketLauncher so it knows where to spawn
+    GameObject m_goLauncher;
 
+    // The timer for the rocket to delete
+    // NOTE: Only for testing, in game a rocket should 
+    // always collide with something, making this obselete
     public float m_fLifespan;
 
+    // Speed that the rocket travels 
     public float m_fRocketSpeed;
+
+    Vector3 m_v3MoveDirection;
     // Use this for initialization
     void Start ()
     {
-		
-	}
+        m_goLauncher = GameObject.FindGameObjectWithTag("RocketLauncher");
+        m_rbRocket = GetComponent<Rigidbody>();
+        m_v3MoveDirection = m_goLauncher.transform.forward;
+    }
 
     private void FixedUpdate()
     {
-        // Bullet recieves direction from the player
-        m_rbRocket.AddForce(transform.forward * m_fRocketSpeed /** Time.deltaTime*/);
         m_fLifespan -= Time.deltaTime;
 
         if (m_fLifespan < 0)
         {
             Destroy(gameObject);
         }
+
+        // Bullet recieves direction from the player
+        m_rbRocket.velocity = m_v3MoveDirection * m_fRocketSpeed;
+        
 
     }
 }
--- Pickled Road-Rage/Assets/Toy Wars/Scripts/Soldier/Weapon/RocketLauncher/RocketLauncher.cs
+++ Pickled Road-Rage/Assets/Toy Wars/Scripts/Soldier/Weapon/RocketLauncher/RocketLauncher.cs
@@ -2,51 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RocketLauncher : Weapon
+public class RocketLauncher : MonoBehaviour
 {
     // Prefab for the Rocket object
     public GameObject m_gRocketPrefab;
     // The object that the rocket should spawn from 
     public GameObject m_gRocketSpawn;
 
-    //
-    GameObject m_goRocket;
-
-    public float m_fSpawnRadius;
-
-    public float m_fRocketLifespan;
-
-    Rigidbody m_rbRocket;
-
-    // Use this for initialization
-    void Awake()
+    public void Fire()
     {
-        
-    }
-	
-	// Update is called once per frame
-	void Update()
-    {
-        if (m_fRocketLifespan <= 0)
-        {
-            Destroy(m_goRocket);
-        }
-        else
-        {
-            m_fRocketLifespan -= 1;
-        }
-
-	}
-
-
-    public override void Fire()
-    {
-
         // Instanciate a new Rocket Prefab
         float spawn_angle = Random.Range(0, 2 * Mathf.PI);
         Vector3 spawn_direction = new Vector3(Mathf.Sin(spawn_angle), 0, Mathf.Cos(spawn_angle));
-        spawn_direction *= m_fSpawnRadius;
-        m_goRocket = Instantiate(m_gRocketPrefab, m_gRocketSpawn.transform.position, Quaternion.identity);
+        GameObject m_goRocket = Instantiate(m_gRocketPrefab, m_gRocketSpawn.transform.position, Quaternion.identity);
         m_goRocket.transform.Rotate(0, Random.Range(m_gRocketSpawn.transform.rotation.eulerAngles.y, m_gRocketSpawn.transform.rotation.eulerAngles.y), 0);
     } 
 }
