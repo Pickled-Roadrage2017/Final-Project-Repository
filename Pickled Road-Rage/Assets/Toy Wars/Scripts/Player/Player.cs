df61fdde35d21ea0bcb9b05343374223f3ff798f
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2017-10-02 00:47:42 +1100

    Added Soldier spawning for Callan's soldiers, Started on a turn manager for the soldiers.

--- Pickled Road-Rage/Assets/Toy Wars/Scripts/Player/Player.cs
+++ Pickled Road-Rage/Assets/Toy Wars/Scripts/Player/Player.cs
@@ -16,20 +16,49 @@ public class Player : MonoBehaviour
     [Tooltip("Current soldiers turn, out of all the players soldiers.")]
     public int m_nSoldierTurn; // ASK RICHARD ABOUT SEEING IN INSPECTOR.
 
+    // public gameobject for the soldier prefab.
+    [Tooltip("The prefab for the Soldier object.")]
+    public GameObject m_gSoldierBlueprint;
+
     // public array of gameobjects for player soldiers.
-    [Tooltip("List of all the soldiers a player has.")]
-    public GameObject[] m_agSoldiers;
+    private GameObject[] m_agSoldierList;
+
+    // pool size. how many soldiers allowed on screen at once.
+    [Range(1, 6)][Tooltip("Specify the max number of solider on each time at once.")]
+    public int m_nPoolSize;
 
     // public gameobject for the Teddy base of this player.
     [Tooltip("The Teddy base for this player.")]
     public GameObject m_gTeddyBase;
+
+    // Current Soliders with a turn.
+    private GameObject m_gCurrentSoldier;
 
     //--------------------------------------------------------------------------------------
     // initialization.
     //--------------------------------------------------------------------------------------
     void Awake()
     {
+        // initialize bullet list with size.
+        m_agSoldierList = new GameObject[m_nPoolSize];
 
+        // Go through each bullet.
+        for (int i = 0; i < m_nPoolSize; ++i)
+        {
+            // Instantiate and set active state.
+            m_agSoldierList[i] = Instantiate(m_gSoldierBlueprint);
+            m_agSoldierList[i].SetActive(false);
+        }
+
+        // REDO // REDO // REDO
+        // Allocate soliders to the pool.
+        GameObject gSoldier1 = AllocateSoldier(); // Allocating 2 at the start for now until..
+        GameObject gSoldier2 = AllocateSoldier(); // ..we have a better idea of solider spawning.
+
+        // Spawn at the teddy base.
+        gSoldier1.transform.position = m_gTeddyBase.transform.position;
+        gSoldier2.transform.position = m_gTeddyBase.transform.position;
+        // REDO // REDO // REDO
     }
 
     //--------------------------------------------------------------------------------------
@@ -40,18 +69,55 @@ public class Player : MonoBehaviour
         // Check if it is this players turn.
         if (m_nPlayerNumber == TurnManager.m_snCurrentTurn)
         {
-            // Soldier Manager.
-            SoldierManager();
+            // Update the soldier.
+            m_nSoldierTurn = SoldierTurnManager(); // ASK RICHARD.
         }
     }
 
     //--------------------------------------------------------------------------------------
-    // SoldierManager: Function that will manager which soldier the player is able to 
+    // AllocateSoldier: Allocate soldiers to the pool.
+    //
+    // Return:
+    //      GameObject: Current gameobject in the pool.
+    //--------------------------------------------------------------------------------------
+    GameObject AllocateSoldier()
+    {
+        // For each in the pool.
+        for (int i = 0; i < m_nPoolSize; ++i)
+        {
+            // Check if active.
+            if (!m_agSoldierList[i].activeInHierarchy)
+            {
+                // Set active state.
+                m_agSoldierList[i].SetActive(true);
+
+                // return the bullet.
+                return m_agSoldierList[i];
+            }
+        }
+
+        // if all fail return null.
+        return null;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // SoldierTurnManager: Function that will manager which soldier the player is able to 
     //                 use per turn.
     //--------------------------------------------------------------------------------------
-    void SoldierManager()
+    int SoldierTurnManager()
     {
-        // TODO.
+        // new int turn.
+        int nTurn = 0;
+
+        // Go up one soldiers turn.
+        nTurn += 1;
+
+        // Go back to the start of the list
+        if (nTurn > m_agSoldierList.Length)
+            nTurn = 1;
+
+        // return turn number.
+        return nTurn;
     }
 
     //--------------------------------------------------------------------------------------
