60d4672efa2b64471dda98bbbf7fddae8cf181d6
CallanDavies <callan.davies1@gmail.com>
2017-10-24 17:29:21 +1100

    Added: Some prefabs for testing and some WIP scripts for the future RocketLauncher firing function.
    - TestingGreenSoldier, TestingRocket and TestingTanSoldier are not fully functional. 
    - The RocketLauncher.cs and Rocket.cs are unchanged, as they are to be completely replaced with the Testing Scripts when they are functional.

---
+++ Pickled Road-Rage/Assets/Toy Wars/Scripts/Soldier/Weapon/RocketLauncher/TestingLauncher.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestingLauncher : MonoBehaviour
+{
+    [Header("Firing Variables")]
+    // Minimum power for a shot
+    [Tooltip("Minimum charge for the Charge, be sure that this matches the 'min value' variable in the Sliders inspector")]
+    public float m_fMinCharge = 1f;
+
+    // speed for the moving of the target line
+    public float m_fAimSpeed;
+    // Float for Max Charge
+    [Tooltip("Maximum charge for the Charge, be sure that this matches the 'max value' variable in the Sliders inspector")]
+    public float m_fMaxCharge = 2;
+    // speed of the charge variable
+    public float m_fChargeSpeed;
+    // The furthest the target can go.
+    public float m_fMaxLength;
+    // Prefab for the Rocket object.
+    [LabelOverride("Rocket")]
+    [Tooltip("Prefab for instantiating the rockets")]
+    public GameObject m_gRocketBlueprint;
+
+    [LabelOverride("RocketLauncher Tilt")]
+    [Range(0, -100)]
+    [Tooltip("This tilts the RocketLauncher, which in turn makes the rockets arc deeper or shallower")]
+    public float m_fRocketXRot;
+
+    // float variable passed on for the firing from the Soldier
+    [HideInInspector]
+    public float m_fCharge = 0;
+
+    // Boolean for the locking of fire if a rocket is still alive
+    [HideInInspector]
+    public bool m_bRocketAlive;
+
+    // Pool for the Rockets (should always be 1)
+    private int m_nPoolsize = 1;
+
+    // An array of Rockets for instantiating rockets
+    private GameObject[] m_agRocketList;
+
+    // Boolean for the slider bar to bounce between m_fMinCharge and m_fMaxCharge
+    private bool m_bIsAscending;
+
+    // boolean for if the soldier is currently charging up a shot
+    private bool m_bChargingShot;
+
+    // where the rocket will be fired towards
+    public Vector3 m_v3CastingLine;
+
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        Cursor.visible = false;
+        m_fChargeSpeed = m_fMinCharge;
+        m_fCharge = m_fMinCharge;
+        m_bChargingShot = false;
+        
+        // initilize rocket list with size
+        m_agRocketList = new GameObject[m_nPoolsize];
+
+        // go through each rocket
+        for (int i = 0; i < m_nPoolsize; ++i)
+        {
+            // Instantiate and set active state
+            m_agRocketList[i] = Instantiate(m_gRocketBlueprint);
+            m_agRocketList[i].SetActive(false);
+        }
+        gameObject.transform.Rotate(m_fRocketXRot, 0, 0);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void SpawnBullet()
+    {
+        if (m_bChargingShot)
+        {
+            // re-Initilise all Variables of the rocket
+            GameObject gRocket = Allocate();
+            if (gRocket.activeInHierarchy)
+            {
+                // Resets all of gRockets current values to their initial values
+                m_bRocketAlive = true;
+                gRocket.GetComponent<TestingRocket>().m_gSpawnPoint = gameObject;
+                gRocket.GetComponent<Rigidbody>().position = gRocket.GetComponent<TestingRocket>().m_gSpawnPoint.transform.position;
+                gRocket.GetComponent<TestingRocket>().transform.position = gRocket.GetComponent<TestingRocket>().m_gSpawnPoint.transform.position;
+                gRocket.GetComponent<TestingRocket>().m_fPower = m_fCharge;
+                gRocket.GetComponent<TestingRocket>().m_v3Target = m_v3CastingLine;
+                gRocket.GetComponent<TestingRocket>().m_fCurrentActivateTimer = gRocket.GetComponent<TestingRocket>().m_fMaxActivateTimer;
+            }
+            // reset the launchers charge value
+            m_fCharge = m_fMinCharge;
+            // launcher is no longer charging a shot
+            m_bChargingShot = false;
+        }
+
+    }
+    public void Fire(EMouseFiringState eMouseState)
+    {
+        if(eMouseState == EMouseFiringState.EMOUSE_DOWN)
+        {
+            m_v3CastingLine = transform.position + transform.forward;
+        }
+
+        if (eMouseState == EMouseFiringState.EMOUSE_HELD)
+        {
+            Debug.DrawLine(transform.position, m_v3CastingLine);
+            m_bChargingShot = true;
+
+            if (m_bIsAscending && m_v3CastingLine.magnitude <= m_fMaxLength)
+            {
+                m_v3CastingLine += transform.forward * m_fAimSpeed * Time.deltaTime;
+                
+                if (m_v3CastingLine.magnitude >= m_fMaxLength)
+                {
+                    m_bIsAscending = false;
+                }
+            }
+            else
+            {
+                m_bIsAscending = false;
+                m_v3CastingLine -= transform.forward * m_fAimSpeed * Time.deltaTime;
+                if (m_v3CastingLine.magnitude <= m_fMaxLength)
+                {
+                    m_bIsAscending = true;
+                }
+            }
+        }
+        else if (m_bChargingShot)
+        {
+            SpawnBullet();
+        }
+    }
+    //--------------------------------------------------------------------------------------
+    // Allocate: For accessing a rocket that isn't currently active in m_agRocketList 
+    //           
+    // Returns: A Rocket from m_agRocketList
+    //
+    //--------------------------------------------------------------------------------------
+    GameObject Allocate()
+    {
+        // Instanciate a new Rocket Prefab
+        for (int i = 0; i < m_nPoolsize; ++i)
+        {
+            //check if active
+            if (!m_agRocketList[i].activeInHierarchy)
+            {
+                //set active state
+                m_agRocketList[i].SetActive(true);
+
+
+
+                //return the rocket
+                return m_agRocketList[i];
+            }
+        }
+        //if all fail, return null
+        return null;
+    }
+
+
+}
+
+
---
+++ Pickled Road-Rage/Assets/Toy Wars/Scripts/Soldier/Weapon/RocketLauncher/TestingRocket.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//-------------------------------------------------------------------------------------------------
+// Rocket: Inheriting from Weapon.cs. Used for propelling rockets and their collison/damage numbers
+//
+//
+// Author: Callan Davies
+//-------------------------------------------------------------------------------------------------
+public class TestingRocket : Weapon
+{
+    //A timer to stop the rocket from colliding with its Launcher
+    [LabelOverride("Maximum time for Activation")]
+    [Tooltip("Seconds it takes for the missle to not be within collision range of its Launcher")]
+    public float m_fMaxActivateTimer = 2;
+
+    [LabelOverride("Mask for Knockback and/or damage")]
+    [Tooltip("Set this to the Unit layer, so the Rocket doesn't knockback objects that should be stationary")]
+    public LayerMask m_UnitMask;
+
+    // The force that hits back all Unit layered objects
+    [Header("Explosion Variables")]
+    [LabelOverride("Force of Explosion")]
+    [Tooltip("The force that hits back all Unit layered objects")]
+    public float m_ExplosionForce = 1000f;
+
+    [LabelOverride("Direct Hit Modifier")]
+    [Tooltip("The velocity is multiplied by this to reach an acceptable knockback")]
+    public float m_fHitMultiplier = 0.5f;
+
+    // Radius for the Area of Effect Explosion that should follow any Collision
+    [LabelOverride("Radius of Explosion")]
+    [Tooltip("Radius for the Area of Effect Explosion that should follow any Collision")]
+    public float m_fExplosionRadius = 5f;
+
+    // how fast the rocket will reach its target
+    public float m_fSpeed;
+
+    // pointer to the RocketLauncher so it knows where to spawn
+    [HideInInspector]
+    public GameObject m_gSpawnPoint;
+
+    // The direction the Rocket should move
+    [HideInInspector]
+    public Vector3 m_v3Target;
+
+    // Starts at m_fMaxActivateTimer and ticks down to zero, then resetting upon being set Inactive
+    [HideInInspector]
+    public float m_fCurrentActivateTimer;
+
+    // its own rigidbody
+    private Rigidbody m_rbRocket;
+
+    // time it will take for the rocket to reach its target position.
+    private float m_fLerpTime = 0;
+
+    // Use this for initialization
+    void Awake()
+    {
+        // m_gRocketLauncher = m_gSpawnPoint.GetComponent<TestingLauncher>();
+        // set the rockets power variable to the power variable of the Launcher
+        // m_fPower = m_gRocketLauncher.m_fCharge;
+        // get the rockets rigidbody
+        m_rbRocket = GetComponent<Rigidbody>();
+        // set the direction for the rocket to do towards
+        //m_v3MoveDirection = m_gSpawnPoint.transform.forward;
+        // initilize ActivateTimer
+        m_fCurrentActivateTimer = m_fMaxActivateTimer;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        m_fCurrentActivateTimer -= 1;
+        m_fLerpTime += Time.deltaTime * m_fSpeed;
+        if (m_fLerpTime > 1.0f)
+            m_fLerpTime = 1.0f;
+
+        transform.position = Vector3.Lerp(m_gSpawnPoint.transform.position, m_v3Target, m_fLerpTime);
+    }
+
+    //--------------------------------------------------------------------------------------
+    // OnTriggerEnter: When a rocket Collides (Is Trigger)
+    //
+    // Param: other is the other object it is colliding with at call
+    //
+    //--------------------------------------------------------------------------------------
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("COLLISION");
+        // if the rocket can now activate
+        if (m_fCurrentActivateTimer <= 0)
+        {
+
+            if (other.tag == "Soldier")
+            {
+
+                Rigidbody rbTarget = other.GetComponent<Rigidbody>();
+                rbTarget = other.GetComponent<Rigidbody>();
+                rbTarget.AddForce(m_rbRocket.velocity * m_fHitMultiplier, ForceMode.Impulse);
+            }
+
+            // Collect all possible colliders 
+            Collider[] aColliders = Physics.OverlapSphere(transform.position, m_fExplosionRadius, m_UnitMask);
+
+            for (int i = 0; i < aColliders.Length; i++)
+            {
+                Rigidbody rbTarget = aColliders[i].GetComponent<Rigidbody>();
+
+                //if it does not have a rigidbody
+                if (!rbTarget)
+                {
+                    continue;
+                }
+
+
+                if (aColliders[i].gameObject.tag == "Soldier")
+                {
+                    // TODO: Explosion particle effect
+                    SoldierActor gtarget = rbTarget.GetComponent<SoldierActor>();
+                    gtarget.TakeDamage(CalculateDamage(aColliders[i].transform.position));
+                    // add explosive force
+                    rbTarget.AddExplosionForce(m_ExplosionForce, transform.position, m_fExplosionRadius, 3.0f, ForceMode.Impulse);
+                }
+                else if (aColliders[i].gameObject.tag == "Teddy")
+                {
+                    Teddy gtarget = rbTarget.GetComponent<Teddy>();
+                    gtarget.TakeDamage(CalculateDamage(aColliders[i].transform.position));
+                }
+
+            }
+            gameObject.SetActive(false);
+           // m_gRocketLauncher.m_bRocketAlive = false;
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    //  CalculateDamage: Calculates the damage so being further from the explosion results in less damage
+    //
+    // Returns: the damage for the Soldiers within range to take
+    //
+    //--------------------------------------------------------------------------------------
+    private float CalculateDamage(Vector3 v3TargetPosition)
+    {
+        // create a vector from the shell to the target
+        Vector3 explosionToTarget = v3TargetPosition - transform.position;
+
+        // Calculated the distance from the shell to the target
+        float explosionDistance = explosionToTarget.magnitude;
+
+        // calculate the proportion of the Maximum distance the target is away
+        float relativeDistance = (m_fExplosionRadius - explosionDistance) / m_fExplosionRadius;
+
+        // Calculate damage as this proportion of the maximum possible damage
+        float fDamage = relativeDistance * m_fDamage;
+
+        fDamage = Mathf.Max(0f, fDamage);
+
+        return fDamage;
+    }
+}
