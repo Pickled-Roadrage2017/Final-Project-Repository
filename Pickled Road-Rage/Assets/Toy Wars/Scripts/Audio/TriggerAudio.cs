9a4a5b3a51fcbb4b9b6ac841c572abd1d2d19116
CallanDavies <callan.davies1@gmail.com>
2017-11-27 15:12:29 +1100

    Xylophone Sounds.

---
+++ Pickled Road-Rage/Assets/Toy Wars/Scripts/Audio/TriggerAudio.cs
@@ -0,0 +1,46 @@
+﻿//--------------------------------------------------------------------------------------
+// Purpose: To play a sound on trigger for objects with no other scripts.
+//
+// Description: Inheriting from MonoBehaviour. Plays a public AudioClip on trigger collision
+//
+// Author: Callan Davies
+//--------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    // the sound to play on Trigger.
+    public AudioClip m_acSound;
+
+    // this objects collider.
+    private Collider m_cCollider;
+
+    // this objects AudioSource.
+    private AudioSource m_aAudioSource;
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
+    {
+        m_cCollider = GetComponent<Collider>();
+        m_aAudioSource = GetComponent<AudioSource>();
+	}
+
+    //--------------------------------------------------------------------------------------
+    // OnTriggerEnter: When a rocket Collides (Is Trigger)
+    //
+    // Param: other is the other object it is colliding with at call
+    //
+    //--------------------------------------------------------------------------------------
+    void OnTriggerEnter(Collider other)
+    {
+        if (m_aAudioSource.isPlaying == false)
+        {
+            m_aAudioSource.PlayOneShot(m_acSound);
+        }
+    }
+}
